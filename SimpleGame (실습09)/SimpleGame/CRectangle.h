690f82601de476c4efde9982bc1e81b4fcb980f4
Yongsun Lee <katarite715@naver.com>
2017-11-29 00:38:53 +0900

    09
    09

---
+++ SimpleGame (실습09)/SimpleGame/CRectangle.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "GameObject.h"
+
+class CRectangle : public GameObject
+{
+private:
+public:
+	CRectangle();
+	CRectangle(OBJECT_TYPE type);
+	CRectangle(Vec3 pos, float size, Vec4 color, OBJECT_TYPE type);
+	virtual ~CRectangle();
+
+	void IsInitialized(Vec3 pos, float size, Vec4 color, OBJECT_TYPE type);
+
+
+	virtual void Draw(Renderer *g_Renderer);		// 오브젝트 생성
+	virtual void Update(float fTimeElapsed);		// 프레임 업데이트
+};
+
